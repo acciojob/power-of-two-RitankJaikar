4b3a6e3c027d152e32de95a56b2f8cca5c9ae6a3
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-12-27 11:46:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const isPowerOfTwo = (number) => {
+const isPowerOfTwo = (n) => {
   //your code here
+  if(n<=0) {
+	  return false;
+  }
+
+  while(n !== 1) {
+	  if(n%2 !== 0) {
+		  return false;
+	  }
+	  n = n/2;
+  }
+	
+  return true;
 };
 
 const num = parseInt(prompt("Enter a number"));
